a6353aadf0b8bbc8d627b038e988794990217a18
animestopbus-ctrl <animestopbus@gmail.com>
2026-02-14 17:15:40 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. Tell Telegram to send messages to your Render URL
-python -c "import telebot, os; bot = telebot.TeleBot(os.environ.get('BOT_TOKEN')); bot.remove_webhook(); bot.set_webhook(url=os.environ.get('WEBHOOK_URL') + '/' + os.environ.get('BOT_TOKEN'))"
-
-# 2. Start the Flask application using Gunicorn on Render's assigned port
-gunicorn bot:app --bind 0.0.0.0:$PORT
